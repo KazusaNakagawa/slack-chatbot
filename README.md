ce115a80925b853e09c6ff349e01d9f3aa497393
KazusaNakagawa <kazusanakagawa@gmail.com>
2023-04-09 15:48:55 +0900

    feat: second commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## ChatGPT Bot
+
+### 手順
+
+1. 実行コマンド
+
+  ```bash
+  python slack_chatbot.py
+  ...
+
+  ⚡️ Bolt app is running!
+  ```
+
+2. Slack で Botにメンションして投稿する。
--- slack_chatbot.py
+++ slack_chatbot.py
@@ -1,36 +1,15 @@
-"""
-Slack BotにGPT APIを組み込むには、まずOpenAI APIキーを取得する必要があります。
-OpenAI APIは、有料のWeb APIであり、APIキーを取得するにはOpenAIのWebサイトでアカウント登録を行う必要があります。
-
-次に、Slack Botを開発するためのWebフレームワークとして、slack-boltライブラリを使用することをお勧めします。
-slack-boltライブラリは、SlackのAPIにアクセスするためのPythonライブラリで、Webフレームワークとしての機能も備えています。
-
-以下は、slack-boltライブラリを使用して、Slack BotにGPT APIを組み込むための例です。
-
-下記の例では、openaiライブラリを使用してGPT APIにアクセスしています。
-app_mentionイベントが発生すると、入力されたテキストからプロンプトを抽出し、GPT APIを使用して応答を生成しています。
-
-この例では、davinciエンジンを使用しており、応答の最大トークン数は1024、temperatureは0.7に設定されています。
-これらの値は、必要に応じて変更することができます。
-
-なお、この例ではSocketModeHandlerを使用してBotを起動していますが、必要に応じてWebサーバーを使用することもできます。
-また、GPT APIを使用するために必要なライブラリをインストールする必要があります。詳細については、OpenAIの公式ドキュメントを
-参照してください。
-
-"""
+""" Slack, OPENAI API を使って指定した Slack チャンネルに返答する Bot """
 import certifi
 import os
 import openai
-import logging
 import ssl
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from dotenv import load_dotenv
 
-os.environ["SSL_CERT_FILE"] = certifi.where()
-
 # FIXME: 開発環境のみ使用: 脆弱性あり
+os.environ["SSL_CERT_FILE"] = certifi.where()
 ssl._create_default_https_context = ssl._create_unverified_context
 
 load_dotenv()
@@ -43,30 +22,50 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 if not SLACK_APP_TOKEN and not SLACK_BOT_TOKEN:
     raise
 
+# OpenAI: Prompt params
+ENGINE = 'davinci'
+
 openai.api_key = OPENAI_API_KEY
 app = App(token=SLACK_BOT_TOKEN)
 
+
+def _response_text(prompt):
+    response = openai.Completion.create(
+        engine=ENGINE,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    return response.choices[0].text.strip()
+
+
 @app.event("app_mention")
 def handle_mentions(body, say, logger):
-# async def command_handler(body, say):
+    logger.level = 20 # INFO
+    logger.info({'body': body})
 
-    # prompt = body['event']['text'].split(' ', 1)[1]
-    # response = openai.Completion.create(
-    #     engine="davinci",
-    #     prompt=prompt,
-    #     max_tokens=1024,
-    #     n=1,
-    #     stop=None,
-    #     temperature=0.7
-    # )
-    # text = response.choices[0].text.strip()
-    # say(text)
-    logging.debug("Received an app_mention event")
-    text = body['event']['text']
-    # メッセージに「hello」という文字列が含まれている場合、応答する
-    if 'hello' in text.lower():
-        say('Hello! How can I help you?')
+    try:
+        # はじめの半角スペースをきりとって、テキストのみ抽出
+        prompt = body['event']['text'].split(' ', 1)[1]
+        # スレッドの time-stamp を取得
+        thread_ts = body["event"]["ts"]
+        logger.info({'prompt': prompt})
+    except IndexError as ex:
+        logger.error({
+            'msg': '入力規則がまちがっています. @メンションの後に 半角スペースが必要です.',
+            'ex': ex,
+        })
 
+    text = _response_text(prompt)
+    # スレッドに返信
+    say(text, thread_ts=thread_ts)
+    logger.info({
+        'text': text,
+        'thread_ts': thread_ts,
+        'status': 200,
+    })
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
