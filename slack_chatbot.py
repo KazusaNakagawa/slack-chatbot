a4afa2cbe64b8dfa342ca92cc013a4f8f68a6cca
KazusaNakagawa <kazusanakagawa@gmail.com>
2023-04-09 17:40:40 +0900

    feat: Cut openai into model classes

--- slack_chatbot.py
+++ slack_chatbot.py
@@ -1,12 +1,12 @@
 """ Slack, OPENAI API を使って指定した Slack チャンネルに返答する Bot """
 import certifi
 import os
-import openai
 import ssl
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-from dotenv import load_dotenv
+from models.oepnai import OpenAI
 
 # FIXME: 開発環境のみ使用: 脆弱性あり
 os.environ["SSL_CERT_FILE"] = certifi.where()
@@ -15,31 +15,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 load_dotenv()
 
 # API トークンを設定する
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 if not SLACK_APP_TOKEN and not SLACK_BOT_TOKEN:
     raise
 
-# OpenAI: Prompt params
-ENGINE = 'davinci'
-
-openai.api_key = OPENAI_API_KEY
 app = App(token=SLACK_BOT_TOKEN)
-
-
-def _response_text(prompt):
-    response = openai.Completion.create(
-        engine=ENGINE,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    return response.choices[0].text.strip()
-
 
 @app.event("app_mention")
 def handle_mentions(body, say, logger):
@@ -58,7 +40,7 @@ def handle_mentions(body, say, logger):
             'ex': ex,
         })
 
-    text = _response_text(prompt)
+    text = OpenAI().response_text(prompt)
     # スレッドに返信
     say(text, thread_ts=thread_ts)
     logger.info({
